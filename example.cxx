bd755ced6909b3239d58f7be16ad9566590b1d74
BarackAdida <adida.barack@student.moringaschool.com>
2025-04-16 00:14:26 +0300

    New c++ file

---
+++ example.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Strathmore University,\n\t";
+    cout << "Our mission is to provide\n\t";
+    cout << "\tAll round quality education in an atmosphere\n";
+    cout << "of freedom and responsibility.";
+
+    return 0;
+}
